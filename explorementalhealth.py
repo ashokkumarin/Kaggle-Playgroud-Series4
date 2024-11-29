73a6f88d3fdb491b4871f1cda5bc3a14a7b82064
ashokkumarin <aashokkumarin@hotmail.com>
2024-11-29 18:47:01 +0530

    Initial Push

---
+++ explorementalhealth.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import warnings
+
+df = pd.read_csv('./data/train.csv')
+df.head()
+
